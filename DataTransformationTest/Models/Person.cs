28fd7127733b7df5172ae260d0acbeb6f4b67dc5
Carter Mann <carter.a.mann@gmail.com>
2021-12-22 15:11:43 -0700

    created person model

---
+++ DataTransformationTest/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransformationTest.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public bool isFemale { get; set; }
+        public bool isStudent { get; set; }
+        public bool isEmployee { get; set; }
+    }
+}
