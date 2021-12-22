e3a06bf6921500de14aae03c4a65127301c1c3e8
Carter Mann <carter.a.mann@gmail.com>
2021-12-22 15:35:52 -0700

    basic read of input file

--- DataTransformationTest/Program.cs
+++ DataTransformationTest/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
 
 namespace DataTransformationTest
 {
     class Program
     {
+        public static readonly string inputFileFolder = "C:\\Temp\\Data\\";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputFile = inputFileFolder + "SampleInputFile.txt";
+            if (File.Exists(inputFile)) {
+                var inputText = File.ReadAllText(inputFile);
+                Console.WriteLine(inputText);
+                Console.Write("Press any key to continue...");
+                Console.ReadKey(true);
+            } else
+            {
+                Console.WriteLine("File read unsuccessful.");
+                Console.Write("Press any key to continue...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
